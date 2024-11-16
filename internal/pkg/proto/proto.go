4836468d20d6241f289c53cdaa0cc3d083aabd33
Oleg <osperelygin@mail.ru>
2024-11-16 16:22:42 +0300

    refactor: change proto package file structure

--- internal/pkg/proto/proto.go
+++ internal/pkg/proto/proto.go
@@ -12,6 +12,118 @@ import (
 	"github.com/howeyc/crc16"
 )
 
+type ModuleID uint8
+
+const (
+	CheckModuleID   ModuleID = 0xF0
+	ControlModuleID ModuleID = 0x01
+)
+
+const (
+	RadioTelemetryModuleID ModuleID = 0x21 + iota*0x10
+	CommunicationModule
+	IMUModuleID
+	GNSSModuleID
+	NavigationModuleID
+	DepthMeterModuleID
+	LidarModuleID
+)
+
+type MessageID uint8
+
+const (
+	ReadingModeA = 0x11 + iota
+	ReadingModeB
+	ReadingModeC
+	WritingModeA
+	WritingModeB
+	WritingModeC
+)
+
+const (
+	headerSize       = 2
+	sytemByteSize    = 1
+	moduleIDSize     = 1
+	msgIDSize        = 1
+	timestampSize    = 4
+	payloadBytesSize = 1
+	checkSumSize     = 2
+)
+
+const serviceBytesSize = headerSize +
+	sytemByteSize +
+	moduleIDSize +
+	msgIDSize +
+	timestampSize +
+	payloadBytesSize +
+	checkSumSize
+
+const payloadFirstByte = serviceBytesSize - checkSumSize
+
+const (
+	defaultBuffSize    = 512
+	defaultReadRetries = 1024
+)
+
+var header = []byte{0xFA, 0xFA}
+
+const (
+	dummySystemByte byte = 0xFF
+)
+
+// Read ищет в потоке принимаемых байтов синхронизовачный заголовок
+// и затем вычитает фрейм протокола. Возвращает полученный фрейм и ошибку.
+func Read(r io.Reader) ([]byte, error) {
+	return ReadWithLimit(r, defaultReadRetries)
+}
+
+// ReadWithLimit аналогично Read, но с возможностью указать лимит не по умолчанию.
+func ReadWithLimit(r io.Reader, limit int) ([]byte, error) {
+	var (
+		rawData []byte
+		svcBuff = make([]byte, serviceBytesSize)
+	)
+
+	_, err := r.Read(svcBuff[:headerSize])
+	if err != nil {
+		return nil, fmt.Errorf("proto.Read failed: %w", err)
+	}
+
+	for retries := limit; len(rawData) == 0 && retries > 0; retries-- {
+		if !bytes.Equal(header, svcBuff[:headerSize]) {
+			copy(svcBuff[0:headerSize-1], svcBuff[1:headerSize])
+
+			_, err = r.Read(svcBuff[headerSize-1 : headerSize])
+			if err != nil {
+				return nil, fmt.Errorf("proto.Read failed: %w", err)
+			}
+
+			continue
+		}
+
+		_, err = r.Read(svcBuff[headerSize:payloadFirstByte])
+		if err != nil {
+			return nil, fmt.Errorf("proto.Read failed: %w", err)
+		}
+
+		payloadSize := svcBuff[payloadFirstByte-1]
+
+		rawData = make([]byte, serviceBytesSize+payloadSize)
+		copy(rawData, svcBuff[:payloadFirstByte])
+
+		_, err = r.Read(rawData[payloadFirstByte:])
+		if err != nil {
+			return nil, fmt.Errorf("proto.Read failed: %w", err)
+		}
+	}
+
+	if len(rawData) == 0 {
+		return nil, fmt.Errorf("frame not found after %d bytes reading", limit)
+	}
+
+	return rawData, nil
+}
+
 // Pack ...
 func Pack(data any, moduleID ModuleID, msgID MessageID) ([]byte, error) {
 	var (
